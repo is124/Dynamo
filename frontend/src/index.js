49f43fc3c9422b5148cf3f0fe0628aef4c4a1142
Tilak Singh <tilaks484@gmail.com>
2023-07-21 00:54:48 +0530

    .

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
     <ToastContainer position="top-right" autoClose={2000} hideProgressBar />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
