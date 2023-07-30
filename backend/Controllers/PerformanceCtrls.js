44ff9e9f246b88a1a8671678c875f631f3e60a47
Tilak Singh <tilaks484@gmail.com>
2023-07-30 13:40:19 +0530

    CheckWebsite()

--- backend/Controllers/PerformanceCtrls.js
+++ backend/Controllers/PerformanceCtrls.js
@@ -7,45 +7,37 @@ const CheckWebsite = async (req, res) => {
   try {
     let URL = req.body.url;
 
-    // Check if the URL starts with "http://" or "https://"
     if (!URL.startsWith('http://') && !URL.startsWith('https://')) {
       URL = `https://${URL}`;
     }
 
-    // BasicCheck - using Axios
     const agent = new https.Agent({ rejectUnauthorized: false });
-
-    // Measure response time using performance.now()
+    
     const start = performance.now();
     const response = await axios.get(URL, { httpsAgent: agent });
     const end = performance.now();
     const responseTime = end - start;
 
-    // Extract status code
+    
     const statusCode = response.status;
 
-    // Check availability based on status code
+    
     const availability = statusCode >= 200 && statusCode < 300;
 
-    // AdvancedCheck - using Puppeteer
-    // Launch Puppeteer and navigate to the URL
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     const urlToCheck = URL;
 
     await page.goto(urlToCheck);
 
-    // Measure performance metrics using Puppeteer's built-in methods
     const metrics = await page.metrics();
     const performanceTiming = JSON.parse(await page.evaluate(() => JSON.stringify(window.performance.timing)));
 
-    // Extract the necessary performance data
     const loadTime = performanceTiming.loadEventEnd - performanceTiming.navigationStart;
     const firstContentfulPaint = metrics['FirstContentfulPaint'];
     const domContentLoaded = metrics['DomContentLoadedEventEnd'];
     const serverResponseTime = performanceTiming.responseStart - performanceTiming.requestStart;
 
-    // Get SSL certificate information using Puppeteer's page.evaluate()
     const sslCertificate = await page.evaluate(() => {
       const certificate = Array.from(document.querySelectorAll('script'))
         .find(script => script.textContent.includes('var ssl = '));
@@ -60,11 +52,9 @@ const CheckWebsite = async (req, res) => {
       return null;
     });
 
-    // Close the browser
     await browser.close();
 
-    // Send the extracted data back to the client
-    res.send({
+    const parameters = {
       basicCheck: {
         availability,
         responseTime,
@@ -76,7 +66,11 @@ const CheckWebsite = async (req, res) => {
         serverResponseTime,
         sslCertificate,
       },
-    });
+    }
+
+    res.json({data: parameters});
+
+
   } catch (err) {
     console.log("Error :: ", err);
     res.status(500).send("An error occurred");
